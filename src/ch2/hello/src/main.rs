847e6ecb40d7cbad44a07ee20eb8920abc7485fd
sughimura <nxsughi@gmail.com>
2025-01-12 21:44:13 +0900

    feat: update main.rs

--- src/ch2/hello/src/main.rs
+++ src/ch2/hello/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let msg_list = [
+        "勤勉な人の計画は必ず成功に繋がり",
+        "せっかちな人は必ず貧乏へと向かう。"];
+    for msg in msg_list {
+        println!("{}", msg);
+    }
 }
