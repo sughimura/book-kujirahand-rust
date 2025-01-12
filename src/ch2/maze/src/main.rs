c15acdf4f7b533f5a91ce8ef1df93abf560ebec0
sughimura <nxsughi@gmail.com>
2025-01-12 22:01:39 +0900

    feat: add 外枠

--- src/ch2/maze/src/main.rs
+++ src/ch2/maze/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use rand::Rng;
+
+const MAP_N: usize = 25;
+
+ fn main() {
+     let mut rng = rand::thread_rng();
+     let mut maze = [[0; MAP_N]; MAP_N];
+     for n in 0..MAP_N {
+         maze[n][0] = 1;
+         maze[0][n] = 1;
+         maze[n][MAP_N - 1] = 1;
+         maze[MAP_N - 1][n] = 1;
+     }
+
+     let tiles = ["  ", "ZZ"];
+     for y in 0..MAP_N {
+         for x in 0..MAP_N {
+             print!("{}", tiles[maze[y][x]]);
+         }
+         println!();
+     }
 }
